9f8a055e8be41da2765574bbe4015c698aa24832
G Martin <georgemartin1603@yahoo.com>
2020-10-08 04:47:07 -0500

    10/8/2020

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Banner from './components/Banner/Banner';
+import Row from './components/Row/Row';
+import requests from './requests'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Netflix clone</h1>
+    <div className="app">
+      <Banner/>
+      <Row 
+      isLargeRow
+      title="Neflix Originals" 
+      fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      
     </div>
   );
 }
